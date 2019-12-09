eb6ed7ebf6e394095a3df0c1813cf3c590c3d111
sunshane6726 <sunshane6726@naver.com>
2019-12-09 20:59:08 +0900

    Surfing 종속성 error 조심하기 - unsigned __int64 [%zd} x

--- opencv_project/opencv_project/main.cpp
+++ opencv_project/opencv_project/main.cpp
@@ -7,9 +7,10 @@ using namespace cv;
 
 int main() {
 	// 이미지 로드
-	Mat mat_ori = cv::imread("C:/works/OpenCV_works/images/images/original.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-	Mat mat_input = cv::imread("C:/works/OpenCV_works/images/images/input.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+	Mat mat_ori = cv::imread("C:/works/images/images/lena.png", CV_LOAD_IMAGE_GRAYSCALE);
+	Mat mat_input = cv::imread("C:/works/images/images/Lenna.jpg", CV_LOAD_IMAGE_GRAYSCALE);
 
+	// 이미지  원본과 특징점이 있는 그림을 추가하기
 	if (mat_ori.empty() || mat_input.empty())
 	{
 		printf("[오류] 이미지 로드 불가! \n");
@@ -18,16 +19,16 @@ int main() {
 	}
 	// 특징점 찾기
 	cv::SurfFeatureDetector detector;
-	vector<cv::KeyPoint> keypoints1;
-	vector<cv::KeyPoint> keypoints2;
+	vector<cv::KeyPoint> keypoints1, keypoints2;
+	
 	detector.detect( mat_ori, keypoints1);
 	detector.detect( mat_input, keypoints2);
-	
+	printf("원본 이미지 특징점 개수 = [%zd]\n 입력 이미지 특징점 개수 = [%zd]\n", keypoints1.size(), keypoints2.size());
 
-	printf("원본 이미지 특징점 개수 = [%d]/n 입력 이미지 특징점 개수 = [%d]/n", keypoints1.size(), keypoints2.size());
-
+	// unsigned __int64 [%zd] 하고는 관련이 없는 것 같다.
 	//기술자 계산
-
+	// 추가 종속성 추가 features2d2413d.lib 문제 라고 보면된다. 
+	// 추가 종속성 추가 features2d2413d.lib 문제 라고 보면된다. 
 	cv::SurfDescriptorExtractor extractor;
 	Mat descriptor1, descriptor2;
 	extractor.compute(mat_ori, keypoints1, descriptor1);
